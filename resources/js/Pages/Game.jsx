4d797decf08a9141423bb9a30cab5c8499a5af20
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-05-30 20:18:42 +0300

    increase to 3 minimum number of players

--- resources/js/Pages/Game.jsx
+++ resources/js/Pages/Game.jsx
@@ -330,7 +330,7 @@ export default function Game({ auth, gameId, gameCode }) {
         }
         <div className="max-w-4xl mx-auto flex justify-center items-center">
 
-          {(admin.isAdmin && players.length > 1 && isUserInPlayers()) &&
+          {(admin.isAdmin && players.length > 2 && isUserInPlayers()) &&
             <button onClick={startNewRound} className="bg-brightgreen text-white px-4 py-2 rounded-md mr-4">
               <AiOutlineSend />
             </button>
