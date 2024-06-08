8d89d41673a61cca4456ca276309982fba0aeb03
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-06-08 15:13:39 +0300

    fix gmail/email loggin in issue

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => 'These credentials do not match our records. Or you are registered under them via Gmail.',
             ]);
         }
 
